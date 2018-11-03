6d942278d8b6028707382df639df0cc67d0d79b3
Bulwinkel <kelvin@bulwinkel.com>
2018-11-04 07:01:35 +1000

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,38 @@
+import com.android.builder.model.ApiVersion
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.bulwinkel.slemusman"
+        setMinSdkVersion (23)
+        setTargetSdkVersion (28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        named("release") {
+            isMinifyEnabled = false
+            proguardFiles (getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(Deps.kotlin_stdlib_jdk7)
+
+    implementation(Deps.androidx_appcompat)
+    implementation(Deps.androidx_constraintlayout)
+    implementation(Deps.androidx_core_ktx)
+    implementation(Deps.androidx_fragment_ktx)
+
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+}
---
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -0,0 +1,26 @@
+object Deps {
+    val kotlin_version = "1.3.0"
+    val kotlin_stdlib_jdk7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${kotlin_version}"
+
+
+    val androidx_appcompat = "androidx.appcompat:appcompat:1.0.0"
+    val androidx_constraintlayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+
+
+    val androidx_core_ktx = "androidx.core:core-ktx:1.0.0"
+    val androidx_fragment_ktx = "androidx.fragment:fragment-ktx:1.0.0"
+    val androidx_palette_ktx = "androidx.palette:palette-ktx:1.0.0"
+    val androidx_sqlite_ktx = "androidx.sqlite:sqlite-ktx:2.0.0"
+    val androidx_collection_ktx = "androidx.collection:collection-ktx:1.0.0"
+
+    val androidx_lifecycle_ktx = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
+    val androidx_lifecycle_reactivestreams_ktx = "androidx.lifecycle:lifecycle-reactivestreams-ktx:2.0.0"
+
+    val android_arch_navigation_common_ktx = "android.arch.navigation:navigation-common-ktx:1.0.0-alpha06"
+    val android_arch_navigation_fragment_ktx = "android.arch.navigation:navigation-fragment-ktx:1.0.0-alpha06"
+    val android_arch_navigation_runtime_ktx = "android.arch.navigation:navigation-runtime-ktx:1.0.0-alpha06"
+    val android_arch_navigation_testing_ktx = "android.arch.navigation:navigation-testing-ktx:1.0.0-alpha06"
+    val android_arch_navigation_ui_ktx = "android.arch.navigation:navigation-ui-ktx:1.0.0-alpha06"
+
+    val android_arch_work_runtime_ktx = "android.arch.work:work-runtime-ktx:1.0.0-alpha10"
+}
