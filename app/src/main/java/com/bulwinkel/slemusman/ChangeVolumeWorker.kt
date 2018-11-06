51e214dae0c7e56c5b9967e085e6911a016a1fcf
Bulwinkel <kelvin@bulwinkel.com>
2018-11-07 06:29:14 +1000

    Fix volume changing to work for both increasing and decreasing scenarios

--- app/src/main/java/com/bulwinkel/slemusman/ChangeVolumeWorker.kt
+++ app/src/main/java/com/bulwinkel/slemusman/ChangeVolumeWorker.kt
@@ -8,10 +8,14 @@ import androidx.work.WorkManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import timber.log.Timber
+import kotlin.math.abs
 
 private var AudioManager.musicVolume: Int
     get() = getStreamVolume(AudioManager.STREAM_MUSIC)
     set(value) = setStreamVolume(AudioManager.STREAM_MUSIC, value, 0)
+
+private val Int.seconds: Int get() = this * 1000
+private val Int.minutes: Int get() = seconds * 60
 
 class ChangeVolumeWorker(
     private val context : Context,
@@ -23,20 +27,31 @@ class ChangeVolumeWorker(
     override fun doWork(): Result {
 
         //todo - this needs to be configable
-        val targetVolumePercent = .8
+        val targetVolumePercent = .2
         //todo - the duration also needs to be configurable
+        val duration = 30.seconds
+        Timber.d("duration millis = $duration")
         // maybe use shared prefs to store these values
 
         val maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) // max is 15 on simulator
         val currentVolume = audioManager.musicVolume // should be 3 at the moment
         val targetVolume = (maxVolume * targetVolumePercent).toInt()
         Timber.d("targetVolume = $targetVolume")
+
         val diff = targetVolume - currentVolume // assuming 10
-        val duration = 30 * 1000 //seconds
-        val timeBetweenIncrements = duration / diff
+
+        val range = when {
+            diff < 0 -> -1 downTo diff
+            diff > 0 -> 1 until (diff + 1)
+            // return from the function (not this when statement)
+            else -> return Result.SUCCESS
+        }
+
+        // must be absolute incase we are decreasing the volume
+        val timeBetweenIncrements = abs(duration.toFloat() / diff)
         Timber.d("timeBetweenIncrements = $timeBetweenIncrements")
 
-        for (i in 0..diff) {
+        range.forEach { i ->
             audioManager.musicVolume = currentVolume + i
             Timber.d("currentVolume = ${audioManager.musicVolume}")
             Thread.sleep(timeBetweenIncrements.toLong())
@@ -45,7 +60,8 @@ class ChangeVolumeWorker(
 
         Timber.d("""doWork:
             |maxVolume = $maxVolume,
-            |currentVolume = ${audioManager.musicVolume}
+            |targetVolume = $targetVolume
+            |currentVolume = ${audioManager.musicVolume},
             |""".trimMargin())
 
         return Result.SUCCESS
